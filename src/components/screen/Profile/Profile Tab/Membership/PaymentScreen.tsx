4ca2911982182579d2e198ed7aa949575786d375
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-09-18 18:08:26 +1000

    Updated paypal token

--- src/components/screen/Profile/Profile Tab/Membership/PaymentScreen.tsx
+++ src/components/screen/Profile/Profile Tab/Membership/PaymentScreen.tsx
@@ -28,7 +28,7 @@ const PaymentScreen = ({ route, navigation }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer A21AAKXI8L9mXtgajdO-c-ggNmu2fcTrw-SjUdChV9uzA17SSflZ5MJAA_kJzajgTpf54_wCRhohJ9iekSTB8996eu9jiXZbQ`,
+          Authorization: `Bearer A21AAJoEU9NrcJY5Rwp3Ru9w4FUL5udQ0bC5MMeau-HvbiTVx1NuG-IYAk_7U6RhlCRBg6r6QOEUQ9cloPn649-uBSWEf4Nzg`,
         },
         body: JSON.stringify({
           intent: "sale",
